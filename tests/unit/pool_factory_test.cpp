82dd3449aef357333d50b507c6552f747c0fc339
Eden Reich <eden.reich@gmail.com>
2019-10-02 19:57:36 +0200

    there is no real assertions that can be made here, step debugging it is fine enough, for now just disable this test

--- tests/unit/pool_factory_test.cpp
+++ tests/unit/pool_factory_test.cpp
@@ -8,33 +8,31 @@
 using namespace Thread;
 
 
-TEST(ThreadFactoryTest, ItCreatesAThreadPool) {
+TEST(ThreadFactoryTest, DISABLED_ItCreatesAThreadPool) {
 
-    Interfaces::PoolInterface * pool = PoolFactory::make(5);
+    // Check how many threads supported on the current hardware.
+    unsigned int concurentThreadsSupported = std::thread::hardware_concurrency();
 
-    pool->enqueue([](std::thread::id threadId) {
-        std::cout << '\n' << "Picked by thread id: " << threadId << '\n';
-        
-        for (unsigned int i = 0; i < 10; i++) {
-            std::cout << "Running Task 1..." << threadId << '\n';
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-        }
+    // Create the pool.
+    Thread::Interfaces::PoolInterface * pool = Thread::PoolFactory::make(concurentThreadsSupported);
 
-        std::cout << "Task 1 finished!" << '\n';
-    });
+    // Enqueue 10 requests to process, this could be processed simultaneously
+    // it's only push these handlers to the queue, and they will be picked up by the next available thread,
+    // in this case we only have 10 threads in the pool, but you can increase it, depending on your hardware.
+    for (unsigned int request = 1; request <= 10; ++request) {
+        pool->enqueue([=](std::thread::id threadId) {            
+            // Demonstrate a long running process
+            for (unsigned int i = 0; i < 10; i++) {
+                std::cout << "\nProcessing Request Nr. " << request << " by process id " << threadId << '\n';
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            }
 
-    pool->enqueue([](std::thread::id threadId) {
-        std::cout << '\n' << "Picked by thread id: " << threadId << '\n';
+            std::cout << "\nProcessing " << request << " finished!" << '\n';
+        });
+    }
 
-        for (unsigned int i = 0; i < 10; i++) {
-            std::cout << "Running Task 2..." << threadId << '\n';
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
+    // If for whatever reason the webserver cannot exit the loop,
+    pool->shutdown();
 
-        std::cout << "Task 2 finished!" << '\n';
-    });
-
-    std::cin.get();
-
-    // pool->shutdown();
+    std::cout << "\nPool has been destroyed" << '\n';
 };
